939e1777c27a95ec3c8c74d6e5c9791200af6b22
amirga <amirgafni@gmail.com>
2014-05-23 15:56:48 -0700

    created rails tb app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackeyb::Application.config.secret_token = '5bd8f4f578a8e46bd9a8265f8caa478afd87454e182a944a5b663101926e0979b45ef079df16a70d4df59de19aa1957def062b8279fba051fac8115084ddb38d'
